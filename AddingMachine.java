2cddf78d919923b2ff71892461626243dca17d4c
Nick Vazquez <nickvazquez@Nicks-MacBook-Pro.local>
2020-10-07 21:19:34 -0700

    Methods completed

--- AddingMachine.java
+++ AddingMachine.java
@@ -3,24 +3,45 @@ package cse360assignment02;
 public class AddingMachine {
   private int total;
   
+  private String transactions = "0";
+  
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
-  }
+    total = total + value;
+    String newValue = Integer.toString(value);
+    
+    transactions = transactions + " + " + newValue;
+
+}
 
   public void subtract (int value) {
-  }
+  total = total - value;
+    String newValue = Integer.toString(value);
+  transactions = transactions + " - " + newValue;
+}
 
   public String toString () {
-    return "";
+    return transactions;
+
+
   }
 
   public void clear() {
+      transactions = "0";
   }
+
+
 }
+
+
+
+
+
+
